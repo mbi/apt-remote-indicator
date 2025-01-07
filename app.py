d9a9666df6c547c0781684f4659d6286aeb949ed
Marco Bonetti <marco@cruncher.ch>
2025-01-07 13:54:56 +0100

    Last update time, handle ssh agent sockets

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import configparser
+import logging
 import os
 import re
+import shlex
 import subprocess
 import sys
 
 import gi
 from paramiko import AutoAddPolicy, SSHClient
+from systemd.journal import JournalHandler
 
 gi.require_version("Notify", "0.7")  # noqa
 gi.require_version("AppIndicator3", "0.1")  # noqa
@@ -16,6 +19,10 @@ from gi.repository import Gtk as gtk  # noqa
 from gi.repository import Notify as notify  # noqa
 
 APPINDICATOR_ID = "remote-apt-dater"
+
+logger = logging.getLogger(APPINDICATOR_ID)
+logger.addHandler(JournalHandler(SYSLOG_IDENTIFIER=APPINDICATOR_ID))
+logger.setLevel(logging.INFO)
 
 
 class App(object):
@@ -30,12 +37,28 @@ class App(object):
         )
 
         self._indicator.set_attention_icon_full("updating.svg", "Updating")
+        self._ssh_agent_locked = False
+        logger.info("Startup complete")
+        self._last_update = None
 
     def build_menu(self, updates=[]):
         menu = gtk.Menu()
 
         for app, version in updates:
-            menu.append(gtk.MenuItem(label=f"{app} {version}"))
+            mi = gtk.MenuItem(label=f"{app} {version}")
+            mi.set_sensitive(False)
+            menu.append(mi)
+        else:
+            mi = gtk.MenuItem(label="Up to date")
+            mi.set_sensitive(False)
+            menu.append(mi)
+
+        if self._last_update:
+            updated_time = GLib.DateTime.format(self._last_update, "%c")
+            updated_item = gtk.MenuItem(label=f"Last checked {updated_time}")
+            updated_item.set_sensitive(False)
+            menu.append(updated_item)
+        menu.append(gtk.SeparatorMenuItem.new())
 
         item_update = gtk.MenuItem(label="Check now")
         item_update.connect("activate", self.update)
@@ -45,6 +68,14 @@ class App(object):
             item_upgrade = gtk.MenuItem(label="Upgrade")
             item_upgrade.connect("activate", self.upgrade)
             menu.append(item_upgrade)
+            menu.append(gtk.SeparatorMenuItem.new())
+
+        if self._ssh_agent_locked:
+            item_unlock = gtk.MenuItem(label="Unlock SSH Agent")
+            item_unlock.connect("activate", self.unlock_agent)
+            menu.append(item_unlock)
+
+        menu.append(gtk.SeparatorMenuItem.new())
 
         item_quit = gtk.MenuItem(label="Quit")
         item_quit.connect("activate", gtk.main_quit)
@@ -69,12 +100,13 @@ class App(object):
         return True
 
     def update(self, *args, **kwargs):
-        print("Updating")
+        logger.info("Updating...")
         self._indicator.set_status(appindicator.IndicatorStatus.ATTENTION)
         # print("Updating")
         self._indicator.set_label("", "")
 
         ssh = SSHClient()
+        available_updates = []
         try:
             ssh.load_system_host_keys()
             ssh.set_missing_host_key_policy(AutoAddPolicy())
@@ -86,41 +118,60 @@ class App(object):
                 "sudo apt-get -q -y --ignore-hold --allow-change-held-packages -s dist-upgrade"
             )
             stdout_.channel.recv_exit_status()
-            lines = [
+            available_updates = [
                 re.match(r"Inst (?P<app>\w+) \[(?P<version>[^\]]+)\]", line).groups()
                 for line in stdout_.readlines()
                 if line.startswith("Inst ")
             ]
-            self._indicator.set_menu(self.build_menu(lines))
-            updates_count = len(lines)
+            updates_count = len(available_updates)
             if updates_count:
                 self._indicator.set_label(str(updates_count), str(updates_count))
             else:
                 self._indicator.set_label("", "")
 
             # Avoid looping when called with timeout_add_seconds
-        except Exception:
+        except Exception as e:
             # print("Updating failed, settings locked state")
             self._indicator.set_icon_full(
                 "locked.svg",
                 "Error connecting",
             )
             self._indicator.set_label("", "")
+            self._ssh_agent_locked = True
+
+            logger.warning("Can't connect: " + str(e))
+
         else:
             #  print("Update done")
             self._indicator.set_icon_full(
                 "sleeping.svg",
                 "Update success",
             )
+            self._ssh_agent_locked = False
 
         finally:
+            self._last_update = GLib.DateTime.new_now_local()
             self._indicator.set_status(appindicator.IndicatorStatus.ACTIVE)
-            print("Update done")
+            self._indicator.set_menu(self.build_menu(available_updates))
+
+            logger.info("Update done")
 
         return None
 
     def upgrade(self, *args, **kwargs):
-        proc = subprocess.Popen(self._config["update"]["upgrade_command"].split(" "))
+        logger.info("Running upgrade")
+        proc = subprocess.Popen(
+            shlex.split(self._config["update"]["upgrade_command"], posix=False)
+        )
+        try:
+            outs, errs = proc.communicate()
+        finally:
+            GLib.timeout_add_seconds(1, self.update)
+
+    def unlock_agent(self, *args, **kwargs):
+        cmd = shlex.split(self._config["update"]["unlock_agent_command"], posix=True)
+        logger.info("Running unlock command:" + str(cmd))
+        proc = subprocess.Popen(cmd)
         try:
             outs, errs = proc.communicate()
         finally:
@@ -132,6 +183,8 @@ if __name__ == "__main__":
         config = configparser.ConfigParser()
         config.read(os.path.join(os.path.dirname(__file__), "config.ini"))
 
+        if config["update"].get("ssh_agent_socket"):
+            os.environ["SSH_AUTH_SOCK"] = config["update"].get("ssh_agent_socket")
         App(config).main()
 
     except KeyboardInterrupt:
